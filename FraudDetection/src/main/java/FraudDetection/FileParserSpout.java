e866ce18d7df6536559a570553f4d581c7911ed8
Alessandra <alessandra.fais@gmail.com>
2019-03-13 20:15:46 +0100

    removed  flag and updated metric measurement in spout

--- FraudDetection/src/main/java/FraudDetection/FileParserSpout.java
+++ FraudDetection/src/main/java/FraudDetection/FileParserSpout.java
@@ -1,7 +1,6 @@
 package FraudDetection;
 
 import Constants.FraudDetectionConstants.Field;
-import Meter.SpoutMeterHook;
 import Util.config.Configuration;
 import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
@@ -9,7 +8,6 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichSpout;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
-import org.apache.storm.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,8 +16,6 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
-
-import static Util.config.Configuration.METRICS_ENABLED;
 
 /**
  * The spout is in charge of reading the input data file, parsing it
@@ -41,6 +37,9 @@ public class FileParserSpout extends BaseRichSpout {
 
     private String file_path;
     private String split_regex;
+
+    private long t_start;
+    private long generated;
     private boolean completed;
 
     /**
@@ -52,6 +51,7 @@ public class FileParserSpout extends BaseRichSpout {
     FileParserSpout(String file, String split) {
         file_path = file;
         split_regex = split;
+        generated = 0;
         completed = false;
     }
 
@@ -59,99 +59,62 @@ public class FileParserSpout extends BaseRichSpout {
     public void open(Map conf, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
         LOG.info("[FileParserSpout] Started.");
 
+        t_start = System.nanoTime(); // spout start time in nanoseconds
+
         config = Configuration.fromMap(conf);
         collector = spoutOutputCollector;
         context = topologyContext;
-
-        if (config.getBoolean(METRICS_ENABLED, false)) {
-            context.addTaskHook(new SpoutMeterHook());
-        }
     }
 
     /**
-     * It is called in an infinite loop by design. For this reason after the
-     * generation is completed the method simply calls a sleep (no operation).
+     * The method is called in an infinite loop by design, this means that the
+     * stream is continuously generated from the data source file.
+     * The parsing phase splits each line of the input dataset in 2 parts:
+     * - first string identifies the customer (entityID)
+     * - second string contains the transactionID and the transaction type
      */
     @Override
     public void nextTuple() {
 
-        long t_start = System.nanoTime();
-        long count = 0;
+        File txt = new File(file_path);
+        ArrayList<String> entities = new ArrayList<>();
+        ArrayList<String> records = new ArrayList<>();
 
-        if (completed) {
-            Utils.sleep(1000); // sleep for 1 second
-        } else {
-            //long startTime = System.nanoTime(); // nanoseconds
-            //long endTime = 0; // nanoseconds
-            //long timeElapsed = 0; // milliseconds
-
-            File txt = new File(file_path);
-            ArrayList<String> entities = new ArrayList<>();
-            ArrayList<String> records = new ArrayList<>();
-
-            // generate for at least 60 seconds (60000 ms)
-            //while (timeElapsed < 60000) {
-
-                /*
-                    split each line of the input dataset in 2 parts:
-                    - first string identifies the customer (entityID)
-                    - second string contains the transactionID and the transaction type
-                 */
-                try {
-                    Scanner scan = new Scanner(txt);
-                    while (scan.hasNextLine()) {
-                        String record = scan.nextLine();
-                        String[] line = record.split(split_regex, 2);
-                        entities.add(line[0]);
-                        records.add(line[1]);
-                        count++;
-                        LOG.debug("[FileParserSpout] EntityID: {}   Record: {}", line[0], line[1]);
-                    }
-                    LOG.info("[FileParserSpout] No more lines to read, closing file...");
-                    scan.close();
-                } catch (FileNotFoundException | NullPointerException e) {
-                    LOG.error("The file {} does not exists", file_path);
-                    throw new RuntimeException("The file '" + file_path + "' does not exists");
-                }
-
-            //    endTime = System.nanoTime();
-            //    timeElapsed = (endTime - startTime) / 1000000;
-            //}
-
-            long b_start = System.nanoTime(); // nanoseconds
-            long b_end = 0; // nanoseconds
-            long b_elapsed = 0; // milliseconds
-            long emitted = 0;
-            int sample = 0;
-            // emit records (generate the stream) and measure bw at each second
-            for (int i = 0; i < entities.size(); i++) {
-                if (b_elapsed >= 1000 && sample == 0) { // 1 second
-                    sample++;
-                    LOG.info("[FileParserSpout] Source bandwidth (emission rate): {} elements per second.",
-                            emitted / b_elapsed * 1000);
-                }
-
-                collector.emit(new Values(entities.get(i), records.get(i)));
-                emitted++;
-
-                b_end = System.nanoTime();
-                b_elapsed = (b_end - b_start) / 1000000;
+        // parsing phase
+        try {
+            Scanner scan = new Scanner(txt);
+            while (scan.hasNextLine()) {
+                String[] line = scan.nextLine().split(split_regex, 2);
+                entities.add(line[0]);
+                records.add(line[1]);
+                generated++;
+                LOG.debug("[FileParserSpout] EntityID: {} Record: {}", line[0], line[1]);
             }
-            completed = true;
-
-            long t_end = System.nanoTime();
-            LOG.info("[FileParserSpout] Generated the whole stream of {} elements in {} ms.",
-                    count, (t_end - t_start) / 1000000);  // spout execution time
+            LOG.info("[FileParserSpout] No more lines to read, closing file...");
+            scan.close();
+        } catch (FileNotFoundException | NullPointerException e) {
+            LOG.error("The file {} does not exists", file_path);
+            throw new RuntimeException("The file '" + file_path + "' does not exists");
         }
-    }
 
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-        outputFieldsDeclarer.declare(new Fields(Field.ENTITY_ID, Field.RECORD_DATA));
+        // emit tuples
+        for (int i = 0; i < entities.size(); i++) {
+            collector.emit(new Values(entities.get(i), records.get(i)));
+        }
     }
 
     @Override
     public void close() {
         LOG.info("[FileParserSpout] Terminated.");
+
+        long t_end = System.nanoTime();  // spout stop time in nanoseconds
+        long t_elapsed = (t_end - t_start) / 1000000; // elapsed time in milliseconds
+        LOG.info("[FileParserSpout] Generated {} elements in {} ms, bandwidth is {} elements per second.",
+                generated, t_elapsed, generated / (t_elapsed / 1000));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields(Field.ENTITY_ID, Field.RECORD_DATA));
     }
 }
