50d565cc56e407d152379edccbe2e9cdf282ee8b
Alessandra <alessandra.fais@gmail.com>
2019-04-04 18:11:07 +0200

    Optimized constant definitions for TM

--- TrafficMonitoring/src/main/java/Constants/BaseConstants.java
+++ TrafficMonitoring/src/main/java/Constants/BaseConstants.java
@@ -1,35 +1,24 @@
 package Constants;
 
-import org.apache.storm.Config;
-import org.apache.storm.utils.Utils;
-
 /**
- *
- * @author Maycon Viana Bordin <mayconbordin@gmail.com>
+ * Constants useful for all the applications.
  */
 public interface BaseConstants {
-    String BASE_PREFIX = "storm";
-    
-    interface BaseConf {
-        String SPOUT_THREADS     = "%s.spout.threads";
-        String SPOUT_CLASS       = "%s.spout.class";
-        String SPOUT_PATH        = "%s.spout.path";
-        String SPOUT_PARSER      = "%s.spout.parser";
-        
-        String SINK_THREADS        = "%s.sink.threads";
-        String SINK_CLASS          = "%s.sink.class";
-        String SINK_PATH           = "%s.sink.path";
-        String SINK_FORMATTER      = "%s.sink.formatter";
-        
-        String DEBUG_ON = Config.TOPOLOGY_DEBUG;
+    String HELP = "help";
+
+    interface Execution {
+        String LOCAL_MODE = "local";
+        String REMOTE_MODE = "remote";
+        int DEFAULT_RATE = -1;
+        int RUNTIME_SEC = 120;  // topology is alive for 120 seconds (valid for Storm)
     }
 
     interface BaseComponent {
         String SPOUT = "spout";
         String SINK  = "sink";
     }
-    
-    interface BaseStream {
-        String DEFAULT = Utils.DEFAULT_STREAM_ID;
+
+    interface BaseField {
+        String TIMESTAMP = "timestamp";
     }
 }
--- TrafficMonitoring/src/main/java/Constants/TrafficMonitoringConstants.java
+++ TrafficMonitoring/src/main/java/Constants/TrafficMonitoringConstants.java
@@ -1,40 +1,46 @@
 package Constants;
 
+/**
+ * Constants peculiar of the TrafficMonitoring application.
+ */
 public interface TrafficMonitoringConstants extends BaseConstants {
-    String PREFIX = "tm";
-    
-    interface Conf extends BaseConf {
+    String DEFAULT_PROPERTIES = "/trafficmonitoring/tm.properties";
+    String DEFAULT_TOPO_NAME = "TrafficMonitoring";
+    String BEIJING_SHAPEFILE = "../../data/app/tm/beijing/roads.shp";
+    String DUBLIN_SHAPEFILE = "../../data/app/tm/dublin/roads.shp";
+
+    interface Conf {
         String MAP_MATCHER_SHAPEFILE = "tm.map_matcher.shapefile";
-        String MAP_MATCHER_THREADS = "tm.map_matcher.threads";
         String MAP_MATCHER_LAT_MIN = "tm.map_matcher.lat.min";
         String MAP_MATCHER_LAT_MAX = "tm.map_matcher.lat.max";
         String MAP_MATCHER_LON_MIN = "tm.map_matcher.lon.min";
         String MAP_MATCHER_LON_MAX = "tm.map_matcher.lon.max";
         
-        String SPEED_CALCULATOR_THREADS = "tm.speed_calculator.threads";
-        
         String ROAD_FEATURE_ID_KEY    = "tm.road.feature.id_key";
         String ROAD_FEATURE_WIDTH_KEY = "tm.road.feature.width_key";
+
+        String MAP_MATCHER_THREADS = "tm.map_matcher.threads";
+        String SPEED_CALCULATOR_THREADS = "tm.speed_calculator.threads";
     }
     
     interface Component extends BaseComponent {
-        String MAP_MATCHER = "mapMatcherBolt";
-        String SPEED_CALCULATOR = "speedCalculatorBolt";
+        String MAP_MATCHER = "map_matcher_bolt";
+        String SPEED_CALCULATOR = "speed_calculator_bolt";
     }
     
     interface Field {
         String TIMESTAMP = "timestamp";
         String VEHICLE_ID = "vehicleID";
-        String DATE_TIME  = "dateTime";
-        String OCCUPIED   = "occupied";
-        String SPEED      = "speed";
-        String BEARING    = "bearing";
-        String LATITUDE   = "latitude";
-        String LONGITUDE  = "longitude";
-        String ROAD_ID    = "roadID";
-        String NOW_DATE   = "nowDate";
-        String AVG_SPEED  = "averageSpeed";
-        String COUNT      = "count";
+        String DATE_TIME = "dateTime";
+        String OCCUPIED = "occupied";
+        String SPEED = "speed";
+        String BEARING = "bearing";
+        String LATITUDE = "latitude";
+        String LONGITUDE = "longitude";
+        String ROAD_ID = "roadID";
+        String NOW_DAT = "nowDate";
+        String AVG_SPEED = "averageSpeed";
+        String COUNT = "count";
     }
 
     interface City {
