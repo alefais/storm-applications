76b8491db7f52433c77a6c00659423709730d165
Alessandra <alessandra.fais@gmail.com>
2019-04-06 13:52:54 +0200

    Removed city area bounding box properties

--- TrafficMonitoring/src/main/java/Constants/TrafficMonitoringConstants.java
+++ TrafficMonitoring/src/main/java/Constants/TrafficMonitoringConstants.java
@@ -10,17 +10,18 @@ public interface TrafficMonitoringConstants extends BaseConstants {
     String DUBLIN_SHAPEFILE = "../../data/app/tm/dublin/roads.shp";
 
     interface Conf {
+        String SPOUT_BEIJING = "tm.spout.beijing";
+        String SPOUT_DUBLIN = "tm.spout.dublin";
+
         String MAP_MATCHER_SHAPEFILE = "tm.map_matcher.shapefile";
-        String MAP_MATCHER_LAT_MIN = "tm.map_matcher.lat.min";
-        String MAP_MATCHER_LAT_MAX = "tm.map_matcher.lat.max";
-        String MAP_MATCHER_LON_MIN = "tm.map_matcher.lon.min";
-        String MAP_MATCHER_LON_MAX = "tm.map_matcher.lon.max";
         
         String ROAD_FEATURE_ID_KEY    = "tm.road.feature.id_key";
         String ROAD_FEATURE_WIDTH_KEY = "tm.road.feature.width_key";
 
+        String SPOUT_THREADS = "tm.spout.threads";
         String MAP_MATCHER_THREADS = "tm.map_matcher.threads";
         String SPEED_CALCULATOR_THREADS = "tm.speed_calculator.threads";
+        String SINK_THREADS = "tm.sink.threads";
     }
     
     interface Component extends BaseComponent {
@@ -28,8 +29,7 @@ public interface TrafficMonitoringConstants extends BaseConstants {
         String SPEED_CALCULATOR = "speed_calculator_bolt";
     }
     
-    interface Field {
-        String TIMESTAMP = "timestamp";
+    interface Field extends BaseField {
         String VEHICLE_ID = "vehicleID";
         String DATE_TIME = "dateTime";
         String OCCUPIED = "occupied";
@@ -41,10 +41,44 @@ public interface TrafficMonitoringConstants extends BaseConstants {
         String NOW_DATE = "nowDate";
         String AVG_SPEED = "averageSpeed";
         String COUNT = "count";
+        String MIN_LAT = "min_lat";
+        String MAX_LAT = "max_lat";
+        String MIN_LON = "min_lon";
+        String MAX_LON = "max_lon";
     }
 
     interface City {
         String BEIJING = "beijing";
         String DUBLIN = "dublin";
+    }
+
+    // constants used for parsing Beijing taxi traces
+    interface BeijingParsing {
+        int B_VEHICLE_ID_FIELD = 0; // carID
+        int B_NID_FIELD = 1;
+        int B_DATE_FIELD = 2;
+        int B_LATITUDE_FIELD = 3;
+        int B_LONGITUDE_FIELD = 4;
+        int B_SPEED_FIELD = 5;
+        int B_DIRECTION_FIELD = 6;
+    }
+
+    // constants used for parsing Dublin bus traces
+    interface DublinParsing {
+        int D_TIMESTAMP_FIELD = 0;
+        int D_LINE_ID_FIELD = 1;
+        int D_DIRECTION_FIELD = 2;
+        int D_JOURNEY_PATTERN_ID_FIELD = 3;
+        int D_TIME_FRAME_FIELD = 4;
+        int D_VEHICLE_JOURNEY_ID_FIELD = 5;
+        int D_OPERATOR_FIELD = 6;
+        int D_CONGESTION_FIELD = 7;
+        int D_LONGITUDE_FIELD = 8;
+        int D_LATITUDE_FIELD = 9;
+        int D_DELAY_FIELD = 10;
+        int D_BLOCK_ID_FIELD = 11;
+        int D_VEHICLE_ID_FIELD = 12; // busID
+        int D_STOP_ID_FIELD = 13;
+        int D_AT_STOP_FIELD = 14;
     }
 }
