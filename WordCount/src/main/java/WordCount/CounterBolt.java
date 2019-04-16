758f738e136b9b2c41a56bfcd7ab0f682d1d0c1a
Alessandra <alessandra.fais@gmail.com>
2019-04-16 13:23:21 +0200

    Updated counting phase

--- WordCount/src/main/java/WordCount/CounterBolt.java
+++ WordCount/src/main/java/WordCount/CounterBolt.java
@@ -1,5 +1,7 @@
 package WordCount;
 
+import Constants.WordCountConstants.Field;
+import Util.config.Configuration;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
@@ -14,38 +16,69 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-import static Constants.Fields.*;
-
+/**
+ * Counts words' occurrences.
+ */
 public class CounterBolt extends BaseRichBolt {
     private static final Logger LOG = LoggerFactory.getLogger(CounterBolt.class);
 
-    OutputCollector collector;
+    protected OutputCollector collector;
+    protected Configuration config;
+    protected TopologyContext context;
 
     private final Map<String, MutableLong> counts = new HashMap<>();
 
+    private long t_start;
+    private long t_end;
+    private long processed;
+    private int par_deg;
+
+    CounterBolt(int p_deg) {
+        par_deg = p_deg;     // bolt parallelism degree
+    }
+
     @Override
-    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+    public void prepare(Map stormConf, TopologyContext topologyContext, OutputCollector outputCollector) {
+        LOG.info("[CounterBolt] Started ({} replicas).", par_deg);
+
+        t_start = System.nanoTime(); // bolt start time in nanoseconds
+        processed = 0;               // total number of processed tuples
+
+        config = Configuration.fromMap(stormConf);
+        context = topologyContext;
         collector = outputCollector;
     }
 
     @Override
     public void execute(Tuple tuple) {
+        String word = tuple.getStringByField(Field.WORD);
+        long timestamp = tuple.getLongByField(Field.TIMESTAMP);
 
-        long t_start = System.nanoTime();
+        if (word != null) {
+            MutableLong count = counts.computeIfAbsent(word, k -> new MutableLong(0));
+            count.increment();
 
-        String event = tuple.getStringByField(WORD_EVENT);
-        String word = tuple.getStringByField(WORD_VALUE);
-        MutableLong count = counts.computeIfAbsent(word, k -> new MutableLong(0));
-        count.increment();
+            collector.emit(new Values(word, count.get(), timestamp));
+        }
+        collector.ack(tuple);
 
-        collector.emit(new Values(WORD_COUNT_EVENT, word, count.get()));
+        processed++;
+        t_end = System.nanoTime();
+    }
 
-        long t_end = System.nanoTime();
-        LOG.warn("[Bolt] execute ~ {} ms.", (t_end - t_start) / 1000000); // bolt execution time
+    @Override
+    public void cleanup() {
+        long t_elapsed = (t_end - t_start) / 1000000; // elapsed time in milliseconds
+
+        LOG.info("[CounterBolt] Processed {} tuples in {} ms. " +
+                        "Source bandwidth is {} tuples per second.",
+                processed, t_elapsed,
+                processed / (t_elapsed / 1000));  // tuples per second
     }
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-        outputFieldsDeclarer.declare(new Fields(WORD_COUNT_EVENT, WORD_VALUE, WORD_COUNT));
+        outputFieldsDeclarer.declare(new Fields(Field.WORD, Field.COUNT, Field.TIMESTAMP));
     }
 }
+
