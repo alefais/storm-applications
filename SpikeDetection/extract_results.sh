bfca047a119a17fe56bbb9a14d74f4e0913a7d61
Alessandra <alessandra.fais@gmail.com>
2019-06-19 17:55:41 +0200

    Updated script tests for SD application

--- SpikeDetection/extract_results.sh
+++ SpikeDetection/extract_results.sh
@@ -5,18 +5,18 @@
 
 ############################################### extract results ########################################################
 
-printf "Extracting bandwidth and latency values\n"
+printf "Extracting bandwidth and latency values for SpikeDetection\n"
 
 NTHREADS=32
 
-NSOURCE_MAX=5
+NSOURCE_MAX=8
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
-    NPRED_MAX=$((NTHREADS-nsource-1))
-    for npred in $(seq 1 $NPRED_MAX);
+    NAVG_MAX=$((NTHREADS-nsource-1))
+    for navg in $(seq 1 $NAVG_MAX);
     do
-        printf "extract from tests/output_60s/main_$nsource-$npred-1_-1.log\n\n"
+        printf "extract from tests/output_60s/main_$nsource-$navg-1-1_-1.log\n\n"
 
-	    grep "Average" tests/output_60s/main_$nsource-$ndet-1-1_-1.log | awk  -F'[, ]' '{ print $17 }' >> tests/output_60s/bandwidth_$nsource.txt
+	    grep "Average" tests/output_60s/main_$nsource-$navg-1-1_-1.log | awk  -F'[, ]' '{ print $17 }' >> tests/output_60s/bandwidth_$nsource.txt
     done
 done
--- SpikeDetection/run.sh
+++ SpikeDetection/run.sh
@@ -14,7 +14,7 @@ fi
 
 #################################################### run tests #########################################################
 
-printf "Running Storm tests with rate -1\n"
+printf "Running Storm tests for SpikeDetection with rate -1\n"
 
 NCORES=16
 NTHREADS=32
@@ -22,11 +22,11 @@ NTHREADS=32
 NSOURCE_MAX=8
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
-    NDET_MAX=$((NTHREADS-nsource-nsource-1))
-    for ndet in $(seq 1 $NDET_MAX);
+    NAVG_MAX=$((NTHREADS-nsource-2))
+    for navg in $(seq 1 $NAVG_MAX);
     do
-        printf "storm_test --nsource $nsource --naverage $ndet --ndet 1 --nsink 1 --rate -1\n\n"
+        printf "storm_test --nsource $nsource --naverage $navg --ndet 1 --nsink 1 --rate -1\n\n"
 
-        storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource $ndet 1 1 | tee tests/output_60s/main_$nsource-$ndet-1-1_-1.log
+        storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource $navg 1 1 | tee tests/output_60s/main_$nsource-$navg-1-1_-1.log
     done
 done
