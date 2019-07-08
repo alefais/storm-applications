ef64719579116fb5a114c81c5dfcf2e4c7e9eef7
Alessandra <alessandra.fais@gmail.com>
2019-07-08 22:17:46 +0200

    Updated FD tests: extract bandwidth values

--- FraudDetection/extract_results.sh
+++ FraudDetection/extract_results.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # @author   Alessandra Fais
-# @date     June 2019
+# @date     July 2019
 
 ############################################### extract results ########################################################
 
@@ -9,7 +9,7 @@ printf "Extracting bandwidth and latency values\n"
 
 NTHREADS=32
 
-NSOURCE_MAX=5
+NSOURCE_MAX=4
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
     NPRED_MAX=$((NTHREADS-nsource-1))
@@ -17,6 +17,15 @@ do
     do
         printf "extract from tests/output_60s/main_$nsource-$npred-1_-1.log\n\n"
 
-	    grep "Predictor" tests/output_60s/main_$nsource-$npred-1_-1.log | awk  -F'[, ]' '{ print $20 }' >> tests/output_60s/bandwidth_$nsource.txt
+	    grep "Predictor" tests/output_60s/main_$nsource-$npred-1_-1.log | awk  -F'[, ]' '{ print $20 }' >> tests/output_60s/bandwidth_$nsource-$npred.txt
+    done
+done
+
+for nsource in $(seq 1 $NSOURCE_MAX);
+do
+    NPRED_MAX=$((NTHREADS-nsource-1))
+    for npred in $(seq 1 $NPRED_MAX);
+    do
+        cat tests/output_60s/bandwidth_$nsource-$npred.txt | awk '{ sum += $1 } END { print sum }' >> tests/output_60s/bandwidth.txt
     done
 done
