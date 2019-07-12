39ea088e49cee8aff875b0e50d97fc2ef8976d43
Alessandra <alessandra.fais@gmail.com>
2019-07-12 13:41:04 +0200

    Updated scripts for SD tests: added colored prints and optimized number of test cases

--- SpikeDetection/run.sh
+++ SpikeDetection/run.sh
@@ -14,24 +14,51 @@ fi
 
 #################################################### run tests #########################################################
 
-printf "Running Storm tests for SpikeDetection application\n"
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+NORMAL=$(tput sgr0)
+
+printf "${GREEN}Running Storm tests for SpikeDetection application\n${NORMAL}"
 
 NTHREADS=32
 NSOURCE_MAX=4
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
     NAVG_MAX=$((NTHREADS-nsource-2))
-    for navg in {0..$NAVG_MAX..2};
+    for navg in {0..29..2};
     do
         if [ $navg -eq 0 ];
         then
-            printf "storm_spikedetection --nsource $nsource --naverage 1 --ndetector 1 --nsink 1 --rate -1\n\n"
+            printf "${BLUE}storm_spikedetection --nsource $nsource --naverage 1 --ndetector 1 --nsink 1 --rate -1\n\n${NORMAL}"
 
-            storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource 1 1 1 | tee tests/output_60s/main_$nsource-1-1-1_-1.log
+            if [ $nsource -le 2 ];
+            then
+                storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource 1 1 1 > tests/output_60s/main_$nsource-1-1-1_-1.log
+            else
+                timeout 10m storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource 1 1 1 > tests/output_60s/main_$nsource-1-1-1_-1.log
+            fi
+        elif [ $navg -le $NAVG_MAX ];
+        then
+            printf "${BLUE}storm_spikedetection --nsource $nsource --naverage $navg --ndetector 1 --nsink 1 --rate -1\n\n${NORMAL}"
+
+            if [ $nsource -le 2 ];
+            then
+                storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource $navg 1 1 > tests/output_60s/main_$nsource-$navg-1-1_-1.log
+            else
+                timeout 10m storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource $navg 1 1 > tests/output_60s/main_$nsource-$navg-1-1_-1.log
+            fi
         else
-            printf "storm_spikedetection --nsource $nsource --naverage $navg --ndetector 1 --nsink 1 --rate -1\n\n"
+            printf "${BLUE}storm_spikedetection --nsource $nsource --naverage 29 --ndetector 1 --nsink 1 --rate -1\n\n${NORMAL}"
 
-            storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource $navg 1 1 | tee tests/output_60s/main_$nsource-$navg-1-1_-1.log
+            if [ $nsource -le 2 ];
+            then
+                storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource 29 1 1 > tests/output_60s/main_$nsource-29-1-1_-1.log
+            else
+                timeout 10m storm jar target/SpikeDetection-1.0-SNAPSHOT-jar-with-dependencies.jar SpikeDetection.SpikeDetection data/sensors.dat $nsource 29 1 1 > tests/output_60s/main_$nsource-29-1-1_-1.log
+            fi
         fi
     done
 done
